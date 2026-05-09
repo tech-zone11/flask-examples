016663f81447c440843a13e99e26c4c7364a50e9
Tech Zone <architect.zone11@gmail.com>
2026-05-10 02:39:15 +0530

    Update route for todo list

--- frontend/app.py
+++ frontend/app.py
@@ -12,6 +12,10 @@ def home(error=None):
     current_time = datetime.now().strftime('%H:%M:%S')
     return render_template('index.html', day_of_week=day_of_week, current_time=current_time, error=error)
 
+@app.route('/todo')
+def todo():    
+    return render_template('todo.html')
+
 @app.route('/submit', methods=['POST'])
 def submit():
     error = None
@@ -33,6 +37,20 @@ def get_data():
         return response.json()
     except requests.exceptions.JSONDecodeError:
         return 'error'
+
+@app.route('/submittodoitem', methods=['POST'])
+def submittodoitem():
+    error = None
+    data = dict(request.form)
+    if data["name"] == "" or data["description"] == "":
+        error = "All fields must be entered"
+    else:
+        response = requests.post(BACKEND_URL + '/submittodoitem', json=data)
+        if response.status_code == 500 or response.status_code == 400 or response.status_code == 401:
+           error = "Error Occurred! Please try after some time"
+        else:
+            return 'Todo Item Added Successfully!'
+    return home(error)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=8000,debug=True)
